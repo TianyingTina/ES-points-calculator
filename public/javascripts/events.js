f21a381bd1cc9c40d220263f5522124ad86aa583
TianyingTina <tearyzhang@gmail.com>
2018-08-13 13:08:24 +0800

    Tina's first commit

---
+++ public/javascripts/events.js
@@ -0,0 +1,8 @@
+/**
+ * Created by tianyingzhang on 8/13/18.
+ */
+function checkrate(){
+    var rate_1 = document.getElementById("rate_1").value;
+    console.log("check rate is in!!!");
+    alert(rate_1);
+}
